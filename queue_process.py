0580524827b1ce76b4c187905fabb13dba45ed84
Miguel Filipe <miguelfilipe.dev@gmail.com>
2024-11-21 22:46:34 +0000

    added queue_process.py

---
+++ queue_process.py
@@ -0,0 +1,12 @@
+from collections import deque
+
+stack1 = deque()
+palavras = input()
+palavras = palavras.split(" ")
+for x in palavras:
+    stack1.append(x)
+print(stack1)
+
+for palavra in palavras:
+        if "o" in palavra:
+            print(palavra)
