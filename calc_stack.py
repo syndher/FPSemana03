8e298bf822321faeab3ba24362f2537c4947e1d4
Miguel Filipe <miguelfilipe.dev@gmail.com>
2024-11-21 22:31:00 +0000

    tweaked calc_stack.py

--- calc_stack.py
+++ calc_stack.py
@@ -1,20 +1,17 @@
 from collections import deque
 
 stack1 = deque()
-stack2 = deque()
-numeros = str(input())
+numeros = input()
 numeros = numeros.split(" ")
-
 for x in numeros:
-    stack1.append(x)
+    stack1.append(int(x))
 print(stack1)
 
 for i in range(int(x)):
     asd = stack1.pop()
-    asd = int(asd)
     abc = asd ** 2
     print(abc)
-        
+
 
 
 
