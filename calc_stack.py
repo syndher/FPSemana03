309e4b6be447098c7ac2a0fd9f1863fc170f1d0e
Miguel Filipe <miguelfilipe.dev@gmail.com>
2024-11-21 22:20:51 +0000

    added everything

---
+++ calc_stack.py
@@ -0,0 +1,20 @@
+from collections import deque
+
+stack1 = deque()
+stack2 = deque()
+numeros = str(input())
+numeros = numeros.split(" ")
+
+for x in numeros:
+    stack1.append(x)
+print(stack1)
+
+for i in range(int(x)):
+    asd = stack1.pop()
+    asd = int(asd)
+    abc = asd ** 2
+    print(abc)
+        
+
+
+
