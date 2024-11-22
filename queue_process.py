ceb971ba8f7145bb80334eff6336cf8555f9a082
Miguel Filipe <miguelfilipe.dev@gmail.com>
2024-11-22 13:30:33 +0000

    tweaked queue_process

--- queue_process.py
+++ queue_process.py
@@ -1,11 +1,15 @@
 from collections import deque
 
 stack1 = deque()
+stack2 = deque()
 palavras = input()
 palavras = palavras.split(" ")
 for x in palavras:
     stack1.append(x)
-print(stack1)
+for i in palavras:
+    stack2.append(stack1.pop())
+
+print(stack2)
 
 for palavra in palavras:
     if "o" in palavra:
